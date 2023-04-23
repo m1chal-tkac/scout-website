c9be4c843e673cb0305937876824e179e37de1f0
Michal Tkáč <tkaaacmichal@gmail.com>
2023-04-23 09:26:52 +0200

    Feat: Added react

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nashuro.cz",
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), react()]
 });
