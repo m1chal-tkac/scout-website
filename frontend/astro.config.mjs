dce8bdd5d025a30ba9ab22d3ff955918c2ea78dd
Michal Tkáč <tkaaacmichal@gmail.com>
2023-04-23 09:27:59 +0200

    Feat: added prefetch

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import react from "@astrojs/react";
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nashuro.cz",
-  integrations: [tailwind(), sitemap(), react()]
+  integrations: [tailwind(), sitemap(), react(), prefetch()],
 });
