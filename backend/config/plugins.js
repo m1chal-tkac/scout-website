c2d48e3e2ae4fad03ecc0ebae6e5a1770022d770
Michal Tkáč <tkaaacmichal@gmail.com>
2023-04-24 06:58:00 +0000

    Fix: GraphQL added introspection

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -6,11 +6,18 @@ module.exports = ({ env }) => ({
       jwtSecret: crypto.randomBytes(16).toString("base64"),
     },
   },
-  config: {
-    endpoint: "/graphql",
-    shadowCRUD: true,
-    playgroundAlways: true,
-    depthLimit: 5,
-    amountLimit: 100,
+  graphql: {
+    enabled: true,
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+        introspection: true,
+      },
+    },
   },
 });
