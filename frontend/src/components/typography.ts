1a917a05b6cb443655de3ed6691e49cd90eeeefa
Michal Tkáč <tkaaacmichal@gmail.com>
2023-05-07 12:17:39 +0200

    Novinky

--- frontend/src/components/typography.ts
+++ frontend/src/components/typography.ts
@@ -2,4 +2,6 @@ export const h1 = "font-skautbold text-3xl mb-4";
 export const h1_center = "font-skautbold text-3xl mb-4 text-center";
 export const h1_cards_section = "font-skautbold text-3xl mb-8 text-center";
 export const h2 = "font-skautbold my-4 text-2xl";
-export const h3 = "font-skautbold my-4 text-base";
+export const h3 = "font-skautbold my-4 text-xl";
+export const h4 = "font-skautbold my-4 text-lg";
+export const h5 = "font-skautbold my-4 text-base";
