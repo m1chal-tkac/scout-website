2066a3dc7384ebbd0d9577dd55ddd9bff5a6619f
Michal Tkáč <tkaaacmichal@gmail.com>
2023-09-10 17:04:42 +0000

    Fix: updates variable increased even though no webhooks were sent

--- docker/node-relay/index.js
+++ docker/node-relay/index.js
@@ -11,6 +11,8 @@ setInterval(async () => {
   }
 }, 24 * 3600 * 1000);
 
+let timeout;
+
 http
   .createServer(async function (req, res) {
     if (req.headers.authorization !== process.env.NODE_RELAY_TOKEN) {
@@ -20,36 +22,29 @@ http
     }
 
     // rate limiting
-    if (updates < allowed_updates_per_day) webhook();
+    if (updates < allowed_updates_per_day) {
+      if (!timeout) updates++;
+      else clearTimeout(timeout);
 
-    updates++;
+      setTimeout(() => webhook(), 5 * 60 * 1000);
+    }
+
     res.writeHead(200, { "Content-Type": "text/html" });
     res.write("Ok");
     res.end();
   })
   .listen(8080);
 
-let timeout;
-
 function webhook() {
-  if (timeout) clearTimeout(timeout);
-
-  timeout = setTimeout(async () => {
-    await fetch(process.env.GITHUB_REPO + "/dispatches", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/vnd.github+json",
-        Authorization: "Bearer " + process.env.GITHUB_TOKEN,
-      },
-      body: JSON.stringify({
-        event_type: "webhook",
-      }),
-    });
-  }, 5 * 60 * 1000);
+  fetch(process.env.GITHUB_REPO + "/dispatches", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/vnd.github+json",
+      Authorization: "Bearer " + process.env.GITHUB_TOKEN,
+    },
+    body: JSON.stringify({
+      event_type: "webhook",
+    }),
+  });
 }
-
-// trigger webhook 5 minutes after deploy
-setTimeout(async () => {
-  webhook();
-}, 5 * 60 * 1000);
