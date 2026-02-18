2e34115ed17279e75c96144bc9b26f828d3a2f03
Michal Tkáč <tkaaacmichal@gmail.com>
2026-02-18 18:12:38 +0100

    Change sponsors margins

--- frontend/src/components/Cards/FlexWrapper.tsx
+++ frontend/src/components/Cards/FlexWrapper.tsx
@@ -1,3 +1,12 @@
-export default function FlexWrapper(props) {
-  return <div className="flex flex-wrap justify-center">{props.children}</div>;
+interface Props {
+  className?: string;
+  children: React.ReactNode | React.ReactNode[];
+}
+
+export default function FlexWrapper(props: Props) {
+  return (
+    <div className={`flex flex-wrap justify-center ${props.className || ""}`}>
+      {props.children}
+    </div>
+  );
 }
